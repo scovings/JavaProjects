32683fb0991c918f9e68150fc636553305eb8778
sco. <77859647+scovings@users.noreply.github.com>
2025-03-12 20:08:52 +0000

    SIGMA

---
+++ 2D-Platformer/src/main/Game.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Game {
+
+    private GameWindow gameWindow;
+
+    public Game() {
+        gameWindow = new GameWindow();
+    }
+
+}
--- 2D-Platformer/src/main/MainClass.java
+++ 2D-Platformer/src/main/MainClass.java
@@ -3,7 +3,7 @@ package main;
 public class MainClass {
 
     public static void main(String[] args){
-        System.out.println("Nigger!");
+        new Game();
 
     }
 
