50bde9d920c12820a4f99e30a82ba6f1e661921f
sco. <77859647+scovings@users.noreply.github.com>
2025-03-12 22:31:20 +0000

    Start Keyboard Inputs

--- 2D-Platformer/src/main/GamePanel.java
+++ 2D-Platformer/src/main/GamePanel.java
@@ -1,20 +1,41 @@
 package main;
 
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+
 import javax.swing.*;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class GamePanel extends JPanel {
 
+    private MouseInputs mouseInputs;
+    private int xDelta = 0, yDelta = 0;
+
     public GamePanel() {
+
+        mouseInputs = new MouseInputs();
+        addKeyListener(new KeyboardInputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
 
     }
 
 
+    public void changeXDelta(int value) {
+        this.xDelta += value;
+    }
+
+    public void changeYDelta(int value) {
+        this.yDelta += value;
+    }
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.fillRect(100, 100, 200, 50);
+        g.fillRect(100 + xDelta, 100 + yDelta, 200, 50);
 
     }
 
