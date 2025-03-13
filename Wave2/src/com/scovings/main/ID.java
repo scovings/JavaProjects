b1166c17635536bfb2cb408c77843a2b91afc9d3
sco. <77859647+scovings@users.noreply.github.com>
2025-03-13 18:08:05 +0000

    javajavajavajavajavajavajavajavajava

---
+++ Wave2/src/com/scovings/main/ID.java
@@ -0,0 +1,9 @@
+package com.scovings.main;
+
+public enum ID {
+
+    Player(),
+    Player2(),
+    Enemy();
+
+}
---
+++ Wave2/src/com/scovings/main/Player.java
@@ -0,0 +1,30 @@
+package com.scovings.main;
+
+import java.awt.*;
+
+import java.util.Random;
+
+public class Player extends GameObject{
+
+    Random r = new Random();
+
+    public Player(int x, int y, ID id) {
+        super(x, y, id);
+
+
+    }
+
+    public void tick() {
+        x += velX;
+        y += velY;
+
+
+    }
+
+    public void render(Graphics g) {
+        if(id == ID.Player) g.setColor(Color.white);
+        else if(id == ID.Player2) g.setColor(Color.green);
+        g.setColor(Color.white);
+        g.fillRect(x, y, 32, 32);
+    }
+}
